3b627c99d8348db84b8e87327df8fc1f9d3cba83
tech-mindacademy <techmindacademy70@gmail.com>
2026-05-28 17:52:22 +0530

    done

--- utils/email.utils.js
+++ utils/email.utils.js
@@ -2,8 +2,8 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT) || 587,
-  secure: process.env.SMTP_PORT === "465",
+  port: Number(process.env.SMTP_PORT) || 465,
+  // secure: process.env.SMTP_PORT === "465",
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
