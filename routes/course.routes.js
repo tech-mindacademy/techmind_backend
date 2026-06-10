e34e724d1b71c70bdf28bc1dcd0fd7d902504a09
tech-mindacademy <techmindacademy70@gmail.com>
2026-06-10 22:36:42 +0530

    done

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -61,7 +61,7 @@ router.get(
   authorizeRoles("admin"),
   getAdminCoursePreview
 );
-router.get("/proxy-segment", protect, proxySegment);
+router.get("/proxy-segment", proxySegment);
 router.get(
   "/:courseId/sections/:sectionId/lessons/:lessonId/proxy",
   protect,
